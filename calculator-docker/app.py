2f59b56d0d265d23c4079dea6817739ada21ff76
Ubuntu <ubuntu@ip-172-31-17-33.ec2.internal>
2026-02-18 11:32:24 +0000

    added lines in file

--- calculator-docker/app.py
+++ calculator-docker/app.py
@@ -1,22 +1,33 @@
 from flask import Flask, request, jsonify
+import redis
+import os
 
 app = Flask(__name__)
 
+# Redis connection
+redis_host = os.getenv("REDIS_HOST", "redis")
+r = redis.Redis(host=redis_host, port=6379, decode_responses=True)
+
 @app.route("/")
 def home():
-    return "Calculator API Running"
+    return "Calculator + Redis Running"
 
 @app.route("/add")
 def add():
     a = float(request.args.get("a", 0))
     b = float(request.args.get("b", 0))
-    return jsonify({"result": a + b})
+    result = a + b
 
-@app.route("/sub")
-def sub():
-    a = float(request.args.get("a", 0))
-    b = float(request.args.get("b", 0))
-    return jsonify({"result": a - b})
+    # Store last result in Redis
+    r.set("last_result", result)
+
+    return jsonify({"result": result})
+
+@app.route("/last")
+def last():
+    result = r.get("last_result")
+    return jsonify({"last_result": result})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
